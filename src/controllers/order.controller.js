c477aec77ceec71a6670cceb766b45f483ffceb9
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-12-17 23:16:06 +0530

    Change Order Status
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -32,21 +32,61 @@ const createOrder = asyncHandler(async (req, res, next) => {
     .json(new ApiResponse(200, {}, "Order placed successfully"));
 });
 
-export const getOrdersOfUser = async (req, res) => {
+export const getOrdersOfUser = asyncHandler(async (req, res) => {
   const orders = await Order.find({ customerId: req.user._id });
 
   return res
     .status(200)
     .json(new ApiResponse(200, orders, "Orders fetched successfully"));
-};
+});
 
-export const getOrdersById = async (req, res) => {
+export const getOrdersById = asyncHandler(async (req, res) => {
   const { orderId } = req.params;
-  const order = await Order.findById({ _id : orderId });
+  const order = await Order.findById({ _id: orderId });
 
   return res
     .status(200)
     .json(new ApiResponse(200, order, "Order fetched successfully"));
-};
+});
+
+export const getAllOrders = asyncHandler(async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    return res
+      .status(200)
+      .json(new ApiResponse(200, orders, "Orders fetched successfully"));
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    return res
+      .status(500)
+      .json(new ApiResponse(500, {}, "Internal Server Error"));
+  }
+});
+
+export const changeOrderStatus = asyncHandler(async (req, res) => {
+  const { orderId, newStatus } = req.body;
+  try {
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return new ApiError(404, "Order not found");
+    }
+
+    // Update the order status
+    order.status = newStatus;
+
+    // Save the updated order
+    await order.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, order, "Order status updated successfully"));
+  } catch (error) {
+    console.error("Error fetching order:", error);
+    return res
+      .status(500)
+      .json(new ApiResponse(500, error, "Internal Server Error"));
+  }
+});
 
 export { createOrder };
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import auth from "../middlewares/auth.js";
-import { createOrder, getOrdersById, getOrdersOfUser } from "../controllers/order.controller.js";
+import { changeOrderStatus, createOrder, getAllOrders, getOrdersById, getOrdersOfUser } from "../controllers/order.controller.js";
+import admin from "../middlewares/admin.js";
 
 const orderRouter = express.Router();
 
 orderRouter.get("/", auth, getOrdersOfUser);
+orderRouter.get("/all-orders", [auth, admin], getAllOrders);
+orderRouter.post("/order/status", [auth, admin], changeOrderStatus);
 orderRouter.get("/:orderId", auth, getOrdersById);
 orderRouter.post("/place-order", auth, createOrder);
 
