b5ca49781a65a1bc72a21278d323e240f9747ab7
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-12-19 21:05:10 +0530

    Implemented Socket.IO
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+const httpServer = createServer(app);
 
 app.use(
   cors({
@@ -16,6 +19,26 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('What is socket', socket);
+  console.log('Socket is active to be connected')
+
+  socket.on('changeStatus', (payload) => {
+    console.log("What is payload", payload);
+    io.emit('changeStatus', payload);
+  })
+});
+
+app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
+
 //routes import
 import userRouter from "./routes/user.route.js";
 import productRouter from "./routes/product.route.js";
@@ -28,4 +51,4 @@ app.use("/api/v1", productRouter);
 app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/orders", orderRouter);
 
-export { app };
+export { httpServer };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import {httpServer} from './app.js'
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    httpServer.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ - Server is listening on http://localhost:${process.env.PORT}`);
     })
 })
